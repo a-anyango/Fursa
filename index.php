8bb827e68c8c2899d6fdf60281264b5a5473af5c
Agnes <agnesanyango30@gmail.com>
2022-07-14 19:51:19 +0300

    Created new pages and navbar

--- index.php
+++ index.php
@@ -4,8 +4,23 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Fursa Home</title>
+	<!--Iconscout CDN-->
+	<link rel="stylesheet" href="https://unicons.iconscout.com/release/v2.1.6/css/unicons.css">
+	<link rel="stylesheet" href="./css/style.css">
 </head>
 <body>
-
+	<nav>
+		<div class="container nav__container">
+			<a href="index.php"> <h4>FURSA</h4></a>
+			<ul class="nav__menu">
+				<li ><a href="index.php">Home</a></li>
+				<li ><a href="about.php">About</a></li>
+				<li ><a href="opportunities.php">Opportunities</a></li>
+				<li ><a href="contact.php">Contact</a></li>
+			</ul>
+			<button id="open-menu-btn"><i class="uil uil-bars"></i></button>
+			<button id="close-menu-btn"><i class="uil uil-multiply"></i></button>
+		</div>
+	</nav>
 </body>
 </html>
