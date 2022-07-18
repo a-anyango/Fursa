8dbd82b436941baee7c9834688e6ac735758d0b8
Agnes <agnesanyango30@gmail.com>
2022-07-18 14:49:03 +0300

    Homepage additions

---
+++ assets/main.js
@@ -0,0 +1,6 @@
+//change navbar styles when scrolling
+
+window.addEventListener('scroll', () => {
+    document.querySelector('nav').classList.toggle
+    ('window-scroll', window.scrollY > 100 )
+})
