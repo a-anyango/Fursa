4297b203e5c03011053d07c9a389165042da084b
Agnes <agnesanyango30@gmail.com>
2022-08-01 08:22:51 +0300

    Changed roles

--- admin/index.php
+++ admin/index.php
@@ -52,18 +52,17 @@ $posts = mysqli_query($connection, $query);
         <button id="show__sidebar-btn" class="sidebar__toggle"><i class="uil uil-angle-right-b"></i></button>
         <button id="hide__sidebar-btn" class="sidebar__toggle"><i class="uil uil-angle-left-b"></i></button>
         <aside>
-            <ul>
-                <li>
+                <?php if (isset($_SESSION['user_is_admin'])) : ?>
+                    <li>
                     <a href="add-post.php"><i class="uil uil-pen"></i>
                         <h5>Add Post</h5>
                     </a>
-                </li>
-                <li>
+                    <li>
                     <a href="index.php" class="active"><i class="uil uil-postcard"></i>
                         <h5>Manage Posts</h5>
                     </a>
                 </li>
-                <?php if (isset($_SESSION['user_is_admin'])) : ?>
+                </li> 
                     <li>
                         <a href="add-user.php"><i class="uil uil-user-plus"></i>
                             <h5>Add User</h5>
@@ -71,7 +70,7 @@ $posts = mysqli_query($connection, $query);
                     </li>
                     <li>
                         <a href="manage-users.php"><i class="uil uil-users-alt"></i>
-                            <h5>Manage User</h5>
+                            <h5>Manage Users</h5>
                         </a>
                     </li>
                     <li>
@@ -88,7 +87,7 @@ $posts = mysqli_query($connection, $query);
             </ul>
         </aside>
         <main>
-            <h2>Manage Users</h2>
+            <h2>Manage Posts</h2>
             <?php if (mysqli_num_rows($posts) > 0) : ?>
                 <table>
                     <thead>
